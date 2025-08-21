b4632ee059f1f0da799e4f75f318f9dae2076ed7
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-21 21:45:56 +0300

    Task 1

--- alx-movie-app/eslint.config.mjs
+++ alx-movie-app/eslint.config.mjs
@@ -1,7 +1,19 @@
+import withPWAInit from "@ducanh2912/next-pwa";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+/* @type {import('next').NextConfig} */
+const withPWA = withPWAInit ({
+  dest: 'public'
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com']
+  }
+}
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -13,4 +25,6 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default withPWA({
+  ...nextConfig
+})
