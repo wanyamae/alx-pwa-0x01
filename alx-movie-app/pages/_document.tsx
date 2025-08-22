f628ababa104d025f94c2218a76ffbea281dbff2
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-22 06:40:25 +0300

    Completed task 3

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#0070f3"/>
       <body className="antialiased">
         <Main />
         <NextScript />
